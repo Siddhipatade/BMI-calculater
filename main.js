3a7c07ea0358f3caaf39ee077f766ab257d7f0d6
Siddhipatade <patadesiddhi20@gmail.com>
2023-06-22 20:16:29 +0530

    initial commit

--- main.js
+++ main.js
@@ -1,35 +1,33 @@
-var weight, height, measure, bmi, error ;
+var weight, height, measure, bmi, error;
 
 function calculate() {
-	weight = document.getElementById("weight").value;
-	height = document.getElementById("height").value;
-	error = "Please enter some values";
-	height /= 100;
-	height *= height;
-	bmi = weight/height;
-	bmi = bmi.toFixed(1);
+    weight = document.getElementById("weight").value;
+    height = document.getElementById("height").value;
+    error = "Please enter valid values";
+    height /= 100;
+    height *= height;
+    bmi = weight / height;
+    bmi = bmi.toFixed(1);
 
-	if (bmi <= 18.4) {
-		measure = "Your BMI is " + bmi + " which means " + "you are Underweight";
-	} else if (bmi >= 18.5 && bmi <= 24.9) {
-		measure = "Your BMI is " + bmi + " which means " + "You are Normal";
-	} else if (bmi >= 25 && bmi <= 29.9) {
-		measure = "Your BMI is " + bmi + " which means " + "You are Overweight";
-	} else if (bmi >= 30) {
-		measure = "Your BMI is " + bmi + " which means " + "You are Obese";
-	}
-	
+    if (bmi <= 18.4) {
+        measure = "Your BMI is " + bmi + " which means you are Underweight";
+        document.getElementById("results").className = "results-underweight";
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+        measure = "Your BMI is " + bmi + " which means you are Normal";
+        document.getElementById("results").className = "results-normal";
+    } else if (bmi >= 25 && bmi <= 29.9) {
+        measure = "Your BMI is " + bmi + " which means you are Overweight";
+        document.getElementById("results").className = "results-overweight";
+    } else if (bmi >= 30) {
+        measure = "Your BMI is " + bmi + " which means you are Obese";
+        document.getElementById("results").className = "results-obese";
+    }
 
-	if (weight === 0 ) {
-		document.getElementById("results").innerHTML = error;
-	} else if (height === 0){
-		document.getElementById("results").innerHTML = error;
-	}
-	 else {
-
-		document.getElementById("results").innerHTML = measure;
-	}
-	if (weight < 0) {
-		document.getElementById("results").innerHTML = "Negative Values not Allowed";
-	}
+    if (weight === "" || weight === 0 || height === "" || height === 0) {
+        document.getElementById("results").innerHTML = error;
+    } else if (weight < 0 || height < 0) {
+        document.getElementById("results").innerHTML = "Negative values not allowed";
+    } else {
+        document.getElementById("results").innerHTML = measure;
+    }
 }
